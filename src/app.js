38a9c7d7d706dafa790f98e44da6e403b1b757fb
aman kumar <amankumar1142001@gmail.com>
2023-03-09 22:55:46 +0530

    changes done

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -5,63 +7,117 @@ const hbs = require("hbs");
 require("./db/conn");
 const Register = require("./models/registers");
 
-const port = process.env.PORT || 3000;
+const bcrypt = require("bcrypt");
 
+const securePassword = async (password) => {
+  const passwordHash = await bcrypt.hash(password, 10);
+  console.log(passwordHash);
+
+  const passwordMatch = await bcrypt.compare(password, passwordHash);
+  console.log(passwordMatch);
+};
+
+// securePassword("Aman@1234"); // It changes everytime when compiled.
+// $2b$10$RY4rxmsLuWEty7A1CJJuzOsm5rdQebH8O5voJcTvkxOgVM.omGbXG
+
+const port = process.env.PORT || 3000;
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 //console.log(static_path);
 app.use(express.static(static_path));
-app.set("view engine","hbs");
+app.set("view engine", "hbs");
 
-app.set("views",template_path); // the original path of views have been changed to template
+app.set("views", template_path); // the original path of views have been changed to template
 hbs.registerPartials(partials_path);
 
-app.get("/",(req,res)=>{
-    res.render("index");
-})
+console.log(process.env.SECRET_KEY);
 
-app.get("/register",(req,res) =>{
-    res.render("register");
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 // create a new user in our database
-app.post("/register",async(req,res) =>{
-    try{
-        const password = req.body.password;
-        const cpassword = req.body.confirm_password;
+app.post("/register", async (req, res) => {
+  try {
+    const password = req.body.password;
+    const cpassword = req.body.confirm_password;
 
-        if(password===cpassword){
-            const registerEmpp = new Register({
-                firstname:req.body.firstname,
-                lastname:req.body.lastname,
-                email:req.body.email,
-                gender:req.body.gender,
-                phone:req.body.phone,
-                age:req.body.age,
-                password:password,
-                confirm_password:cpassword
+    if (password === cpassword) {
+      const registerEmpp = new Register({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        gender: req.body.gender,
+        phone: req.body.phone,
+        age: req.body.age,
+        password: password,
+        confirm_password: cpassword,
+      });
+      console.log("Sucess : " + registerEmpp);
 
+      const token = await registerEmpp.generateAuthToken();
+      console.log("Token : " + token);
 
-            })
-
-            const registered = await registerEmpp.save();
-            res.status(201).render("index");
-            console.log(registered);
-        }else{
-            res.send("Password not matching!");
-        }
-
-    }catch(e){
-        res.status(400).send(e);
+      const registered = await registerEmpp.save();
+      res.status(201).render("login");
+      console.log(registered);
+    } else {
+      res.send("Password not matching!");
     }
-})
+  } catch (e) {
+    res.status(400).send(e);
+    console.log("The error !");
+  }
+});
 
-app.listen(port,()=>{
-    console.log(`Running at port ${port}`);
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    // app.use(express.urlencoded({ extended: true }));
+    // console.log(`${email} and password : ${password}`);
+
+    // check wether the email and password is already present in DB
+    const userEmail = await Register.findOne({ email: email });
+
+    const isMatch = await bcrypt.compare(password, userEmail.password);
+    const token = await userEmail.generateAuthToken();
+    console.log("Token : " + token);
+
+    if (isMatch) {
+      res.status(201).render("index");
+    } else {
+      res.send("Password or Email is invalid!!!");
+    }
+  } catch (e) {
+    res.status(400).send("Invalid login Details");
+  }
+});
+
+// const jwt = require("jsonwebtoken");
+
+// const createToken = async() =>{
+//     const token = await jwt.sign({_id:'64089922b7ddc2c4a1bd8392'},"mynameismankumarpandaandiamstudyinginkiit"); // payload(object)
+//     console.log(token);
+
+//     const userVer = await jwt.verify(token,"mynameismankumarpandaandiamstudyinginkiit")
+//     console.log(userVer);
+// }
+// createToken();
+app.listen(port, () => {
+  console.log(`Running at port ${port}`);
+});
--- src/models/registers.js
+++ src/models/registers.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const empSchema = new mongoose.Schema({
     firstname :{
@@ -36,7 +36,43 @@ const empSchema = new mongoose.Schema({
     confirm_password:{
         type:String,
         required:true
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
+
+})
+//generating tokens
+empSchema.methods.generateAuthToken = async function(){
+    try{
+        console.log(this._id);
+        const token = jwt.sign({_id:this._id.toString()}, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+
+        // console.log(token);
+        return token;
+    }catch(e){
+        res.send("The error is "+e);
+        console.log(e);
     }
+}
+// Hashing the password
+empSchema.pre("save",async function(next){
+    if(this.isModified("password"))
+    {
+        //const passwordHash = await bcrypt.hash(password,10);
+        console.log(`The current password is ${this.password}`);
+        this.password = await bcrypt.hash(this.password,10);
+        console.log(`The current password (Hashing) is ${this.password}`);
+
+        
+    }
+    
+    next();
 
 })
 
